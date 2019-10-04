171dd64adb52c5fc60fa6ae6d1e8900c2aecffd9
Wayan Jimmy <jimmyeatcrab@gmail.com>
2019-10-04 13:11:45 +0700

    Initialize

---
+++ lib/algorithms/binary_search.rb
@@ -0,0 +1,17 @@
+def binary_search_rec(a, key, low, high)
+  return -1 if low > high
+
+  middle = low + ((high - low) / 2)
+
+  if a[middle] > key && a.length > 0
+    return binary_search_rec(a, key, low, middle - 1)
+  elsif a[middle] < key && a.length > 0
+    return binary_search_rec(a, key, middle + 1, high)
+  else
+    return middle
+  end
+end
+
+def binary_search(a, key)
+  return binary_search_rec(a, key, 0, a.length - 1)
+end
