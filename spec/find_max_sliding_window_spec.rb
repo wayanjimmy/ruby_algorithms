431349c6bb8a9c900d1437c65e2d890b10a7ffae
Wayan Jimmy <jimmyeatcrab@gmail.com>
2019-10-05 15:48:26 +0700

    Prettier

--- spec/find_max_sliding_window_spec.rb
+++ spec/find_max_sliding_window_spec.rb
@@ -2,8 +2,7 @@ require 'algorithms/find_max_sliding_window'
 
 RSpec.describe 'Find max sliding window works correctly' do
   it 'returns [2, 2, 1, 6]' do
-    expect(find_max_sliding_window([-4, 2, 5, 1, -1, 6], 3)).to match_array(
-      [2, 2, 1, 6]
-    )
+    input = [-4, 2, 5, 1, -1, 6]
+    expect(find_max_sliding_window(input, 3)).to match_array([2, 2, 1, 6])
   end
 end
